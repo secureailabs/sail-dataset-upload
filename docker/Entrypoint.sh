037e82ad3d1288a0b69bee0d90fe1e4e1996c18f
Prawal Gangwar <prawal.gangwar@gmail.com>
2023-05-18 16:07:52 +0530

    disable nginx

--- docker/Entrypoint.sh
+++ docker/Entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-imageName=apiservices
+imageName=dataupload
 
 # Start the nginx server
-nginx -g 'daemon off;' 2>&1 | tee /app/nginx.log &
+# nginx -g 'daemon off;' 2>&1 | tee /app/nginx.log &
 
 # Start the Public API Server
 uvicorn app.main:server --host 0.0.0.0 --port 8000
